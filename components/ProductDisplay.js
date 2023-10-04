bc484e68064d5f438d2bb07460f170e7bff6a578
sego <antoniosegoviaruiz@gmail.com>
2023-10-04 16:41:29 +0200

    Lesson 10 - Communicating events
    Add a method to add items and remove items from cart

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -24,21 +24,28 @@ app.component('product-display', {
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
 
-        <div 
-          v-for="(variant, index) in variants" 
-          :key="variant.id" 
-          @mouseover="updateVariant(index)" 
-          class="color-circle" 
+        <div
+          v-for="(variant, index) in variants"
+          :key="variant.id"
+          @mouseover="updateVariant(index)"
+          class="color-circle"
           :style="{ backgroundColor: variant.color }">
         </div>
-        
-        <button 
-          class="button" 
-          :class="{ disabledButton: !inStock }" 
-          :disabled="!inStock" 
+
+        <button
+          class="button"
+          :class="{ disabledButton: !inStock }"
+          :disabled="!inStock"
           v-on:click="addToCart">
           Add to Cart
         </button>
+        <button
+          class="button"
+          :class="{ disabledButton: cart?.length === 0 }"
+          :disabled="cart?.length === 0" 
+          v-on:click="removeFromCart">
+          Remove from Cart
+          </button>
       </div>
     </div>
   </div>`,
@@ -50,13 +57,16 @@ app.component('product-display', {
         details: ['50% cotton', '30% wool', '20% polyester'],
         variants: [
           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 1 },
         ]
     }
   },
   methods: {
       addToCart() {
-          this.cart += 1
+          this.$emit('add-to-cart', this.variants[this.selectedVariant]?.id);
+      },
+      removeFromCart() {
+          this.$emit('remove-from-cart', this.variants[this.selectedVariant]?.id);
       },
       updateVariant(index) {
           this.selectedVariant = index
